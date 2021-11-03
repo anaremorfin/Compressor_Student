cc9947bdfa25549a21678f69f39af4e24ff9f959
zsantiago8712 <zsantiago8788@icloud.com>
2021-11-02 19:00:23 -0600

    Update binaryBuffer.c

--- src/binaryBuffer.c
+++ src/binaryBuffer.c
@@ -3,12 +3,38 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+bbuffer32 setBuffer32(bbuffer32 data, uint32_t buffer)
+{
+    *data = 0b00000000000000000000000000000000 | buffer;
+
+    return data;
+}
+
+uint32_t getContentBuffer32(bbuffer32 data)
+{
+    return *data;
+}
+
+bbuffer32 newbuffer32(int init_value)
+{
+  bbuffer32 newbuffer32 = malloc(sizeof(uint32_t))
+  *newbuffer32 = init_value;
+  return newbuffer32;
+}
+
+bbuffer32 freeBuffer(bbuffer32 toFree)
+{
+    if(toFree != NULL)
+    {
+        free(toFree);
+        toFree = NULL;
+    }
+    return toFree;
+}
+
 
 char* bufferToString(bbuffer32 data, char* string){
-
-   
-   printf("BYTES->%zu || BITES --> %zu\n", sizeof(*data), sizeof(*data) * 8);
-   
+ 
    for(int size = (sizeof(*data) * 8) - 1; size >= 0; size--){
       
       if((1 << size) & *data)
