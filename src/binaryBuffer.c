182f7be45ac31271603a0a8f65da0220c3b8b74a
anaremorfin <anaremorfin@gmail.com>
2021-11-02 20:50:26 -0600

    Update binaryBuffer.c
    ;

--- src/binaryBuffer.c
+++ src/binaryBuffer.c
@@ -18,7 +18,7 @@ uint32_t getContentBuffer32(bbuffer32 data)
 
 bbuffer32 newbuffer32(int init_value)
 {
-  bbuffer32 newbuffer32 = malloc(sizeof(uint32_t))
+  bbuffer32 newbuffer32 = malloc(sizeof(uint32_t));
   *newbuffer32 = init_value;
   return newbuffer32;
 }
@@ -35,9 +35,9 @@ bbuffer32 freeBuffer(bbuffer32 toFree)
 
 
 char* bufferToString(bbuffer32 data, char* string){
- 
+
    for(int size = (sizeof(*data) * 8) - 1; size >= 0; size--){
-      
+
       if((1 << size) & *data)
          string[((sizeof(*data) * 8) - 1) - size] = '1';
       else
